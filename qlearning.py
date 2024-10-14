4721582b6560691ec0bcb4b2aab63cfb7d597da8
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2024-10-14 09:34:29 +0200

    fix: remove comments for Qlearning

--- qlearning.py
+++ qlearning.py
@@ -83,8 +83,6 @@ class QLearningAgent:
         """
         Compute the action to take in the current state, including exploration.
 
-        Exploration is done with epsilon-greey. Namely, with probability self.epsilon, we should take a random action, and otherwise the best policy action (self.get_best_action).
-
         Note: To pick randomly from a list, use random.choice(list).
               To pick True or False with a given probablity, generate uniform number in [0, 1]
               and compare it with your probability
